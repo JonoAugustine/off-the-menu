c656c798221f0403adef40977176b0d55bd84f9d
Jono <jaugust8@jhu.edu>
2019-11-21 01:07:34 -0500

    theming using 2 theme stylsheets

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,30 +1,110 @@
-const Container = callback => {
-  return E("div").addClass("ui container");
+const theme = {
+  light: {
+    location: "./assets/style/light.css",
+    primary: "#f9f9f9"
+  },
+  dark: {
+    location: "./assets/style/dark.css",
+    primary: "#1f1f1f"
+  },
+  link: function() {
+    return $("#theme-link");
+  },
+  load: function() {
+    // Attempt to load & set theme from local storage
+    const lst = localStorage["theme"];
+    if (typeof lst === "string" && lst.includes("dark")) {
+      this.link().attr("href", this.dark.location);
+    } else {
+      this.link().attr("href", this.light.location);
+    }
+  },
+  toggle: function() {
+    const nTheme = this.link()
+      .attr("href")
+      .includes("light")
+      ? this.dark.location
+      : this.light.location;
+    this.link().attr("href", nTheme);
+    localStorage["theme"] = nTheme.location;
+  }
 };
 
-const Input = {
-  Text: () => {
-    return E("input")
-      .addClass("ui input large mat-input")
+theme.toggle();
+
+/**
+ * Simplified DOM Element creation. Same as `$("<div>")`
+ * but does not need chevrons.
+ * @param {String} tag
+ * @returns A new JQuery DOM Element of the given tag
+ */
+const jqe = (tag, callback) => {
+  const e = $(`<${tag}>`);
+  if (typeof callback === "function") callback(e);
+  return e;
+};
+
+const Container = callback => jqe("div", callback).addClass("ui container");
+
+const Inputs = {
+  Text: callback => {
+    return jqe("input", callback)
+      .addClass("ui input")
       .attr("type", "text");
   }
 };
 
 const Navbar = () => {
-  const base = E("nav").addClass("navbar");
+  const base = jqe("nav").addClass("navbar");
   const inner = Container();
   base.append(inner);
-  const brand = E("h2")
-    .text(Company.name)
+  const brand = jqe("h2")
+    .text(ProjectInfo.name)
     .addClass("brand");
   inner.append(brand);
   return base;
 };
 
 const HomePage = () => {
-  const container = Container().css({ "margin-top": "1em" });
-  const test_input = Input.Text();
-  return container.append(test_input);
+  const wrapper = jqe("div");
+
+  wrapper.append(Navbar);
+
+  const body = Container()
+    .addClass("centered")
+    .css({ "margin-top": "1em" });
+
+  const allergenInput = Inputs.Text();
+  const tagBox = jqe("div").addClass("tag-box");
+  body.append(allergenInput, tagBox);
+
+  wrapper.append(body);
+
+  return wrapper;
 };
 
-$(document).ready(() => show(Navbar));
+/** Root div to manipulate. */
+const root = () => $("#root");
+
+/**
+ * Empties root element and appends child element.
+ * @param {*} component
+ * @returns The root element with given child appended.
+ */
+const render = component =>
+  root()
+    .empty()
+    .append(component);
+
+/***********************
+ **********************/
+
+let currentPage = null;
+
+$(document).ready(() => {
+  currentPage = "home";
+
+  theme.load();
+
+  render(HomePage());
+});
