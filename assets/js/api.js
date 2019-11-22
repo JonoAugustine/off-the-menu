a83447521406458b6ac8ca71669c3418720d78f4
JGen1 <jengenes01@gmail.com>
2019-11-21 19:47:34 -0500

    Retrieved API key for Google Maps API. Created div to test out basic functionality of the maps API.

---
+++ assets/js/api.js
@@ -0,0 +1,7 @@
+function initMap(){
+    var options = {
+        zoom :8,
+        center:{lat: 42.3601, lng:-71.0589}
+        }
+    var map = new google.maps.Map(document.getElementById('map'),options);
+    }
