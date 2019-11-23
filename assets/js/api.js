28a9967cc1bd7d35df3f30071472b5834f1ce1e2
JGen1 <jengenes01@gmail.com>
2019-11-23 12:32:53 -0500

    Incorporated three functions into api.js. First one initializes the map with the area the user is in and the queried Places in a 5 mile radius. Second one is a callback function that retrieves successul queries and creates a marker for each one. Third one is the marker function which places the markers for each location.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,24 +1,61 @@
+var map;
+initMap();
+
 function initMap(){
-    //Map options
-    var options = {
-        zoom :8,
-        center:{lat: 42.3601, lng:-71.0589}
+    // Map options
+    var center = new google.maps.LatLng(37.422,-122.084058);
+    map = new google.maps.Map(document.getElementById('map'),{
+        center: center,
+        zoom:8
+   });
+   //Googles request to find places
+   var request = {
+       location: center,
+       //In meters ~5 miles
+       radius:8047,
+       //Category
+       types: ['cafe']
+   };
+
+   var service = new google.maps.places.PlacesService(map);
+
+   service.nearbySearch(request, callback);
+}
+
+
+//Retrieve and append to array of results
+function callback(results,status){
+    if(status == google.maps.places.PlacesServicesStatus.OK){
+        for(var i = 0; i < results.length; i++){
+            createMarker(results[i]);
         }
-    var map = new google.maps.Map(document.getElementById('map'),options);
-    
-    //Add marker
+    }
+}
+
+function createMarker(place){
+
+    var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
-    position:{lat: 42.4668, lng:-70.9495},
-    map:map,
-    //Change marker icon
-    //icon:''
+        map: map,
+        position: place.geometry.location
+        //Change marker icon
+        // icon:'';
     });
+}
+    //Some stuff I'm trying to figure out:
 
-    var infoWindow = new google.maps.infoWindow({
-    content:'<h1>Lynn MA</h1>'
-    });
+    // var infoWindow = new google.maps.infoWindow({
+    // content:'<h1>Lynn MA</h1>'
+    // });
 
-    marker.addListener('click',function(){
-        infoWindow.open(map,marker);
-    });
+    // marker.addListener('click',function(){
+    //     infoWindow.open(map,marker);
+    // });
+
+//check if geolocation is available
+if (navigator.geolocation) { 
+    navigator.geolocation.getCurrentPosition(function(position){
+      //Retrieve position properties
+      console.log(position);
+    });   
 }
