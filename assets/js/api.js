fa8729e7160272aca23ab22c2404dd02a0385363
Jono <jaugust8@jhu.edu>
2019-11-25 18:19:35 -0500

    formatting

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,31 +2,33 @@ var map;
 var service;
 var infowindow;
 
-
-
 //check if geolocation is available
-if (navigator.geolocation) { 
-    navigator.geolocation.getCurrentPosition(function(position){
-      //Retrieve position properties
-      initMap(position);
-      //Retrieve lat & long
-    });   
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    //Retrieve position properties
+    initMap(position);
+    //Retrieve lat & long
+  });
 }
 
-
 function initMap(position) {
-    console.log(position.coords);
-//   var sydney = new google.maps.LatLng(-33.867, 151.195);
-    
-var userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  console.log(position.coords);
+  //   var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+  var userLocation = new google.maps.LatLng(
+    position.coords.latitude,
+    position.coords.longitude
+  );
   infowindow = new google.maps.InfoWindow();
 
-  map = new google.maps.Map(
-      document.getElementById('map'), {center: userLocation, zoom: 15});
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: userLocation,
+    zoom: 15
+  });
 
   var request = {
-    query: 'Starbucks',
-    fields: ['name', 'geometry']
+    query: "Starbucks",
+    fields: ["name", "geometry"]
   };
 
   var service = new google.maps.places.PlacesService(map);
@@ -41,14 +43,13 @@ var userLocation = new google.maps.LatLng(position.coords.latitude, position.coo
   });
 }
 
-
-
-function createMarker(place){``
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-        //Change marker icon
-        // icon:'';
-    });
+function createMarker(place) {
+  ``;
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+    //Change marker icon
+    // icon:'';
+  });
 }
