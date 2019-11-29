d6d7d8da1530981cc5394f5449c952cd3fc7267b
Jono <jaugust8@jhu.edu>
2019-11-29 01:54:10 -0500

    extracted search result class food item model

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,3 +6,18 @@ const User = {
 const ProjectInfo = {
   name: "AllergicToThat"
 };
+
+/** @returns true if the given object contains the given target. Includes sub-objects. */
+const contains = (target, any) => {
+  for (const k in any) {
+    if (k === target) {
+      return true;
+    } else if (any[k] === target) {
+      return true;
+    } else if (typeof any[k] === "object") {
+      if (contains(target, any[k])) {
+        return true;
+      }
+    } else return false;
+  }
+};
--- assets/js/fdcApi.js
+++ assets/js/fdcApi.js
@@ -1,3 +1,35 @@
+class FdcSearchResult {
+  /**
+   * @param {*} raw
+   * @param {string} store
+   */
+  constructor(raw, store) {
+    store = store.replace(/[^A-Z]/gi, "").toLowerCase();
+    this.request = raw.foodSearchCriteria;
+    this.raw = raw;
+    this.responseCount = raw.totalHits;
+    this.results = raw.foods
+      .filter(fd =>
+        fd.description
+          .replace(/[^A-Z]/gi, "")
+          .toLowerCase()
+          .includes(store)
+      )
+      .map(fd => {
+        return {
+          id: fd.fdcId,
+          description: fd.description.toLowerCase()
+        };
+      });
+  }
+}
+
+class FdcFoodItem {
+  constructor(raw) {
+    this.raw = raw;
+  }
+}
+
 const FdcSpec = {
   key: "dfPdM4ko8xAaxTxfdOAgxdJKbfbPX9hIj1bNBv64",
   baseUri: "https://api.nal.usda.gov/fdc/v1",
@@ -6,34 +38,14 @@ const FdcSpec = {
    *
    * @param {string} itemID The food item's FDC ID
    */
-  itemUri: itemID => `${FdcSpec.baseUri}/${itemID}?api_key=${key}`,
-  /**
-   *
-   * @param {*} raw
-   * @param {string} store
-   * @param {boolean} filtered
-   */
-  SearchResult: function(raw, store, filtered) {
-    this.raw = raw;
-    this.responseCount = raw.totalHits;
-    this.results = filtered
-      ? raw.foods
-          .filter(fd => fd.description.toLowerCase().includes(store)) // TODO Should we filter?
-          .map(fd => ({ ...fd, description: fd.description.toLowerCase() })) // TODO Sort by the number of keyword matches
-      : raw.foods;
-    this.request = raw.foodSearchCriteria;
-    this.pagination = {
-      current: raw.currentPage,
-      total: raw.totalPages
-    };
-  },
+  itemUri: itemID => `${FdcSpec.baseUri}/${itemID}?api_key=${FdcSpec.key}`,
   /**
    * Makes a search request to the FDC API.
    * @param {string} store The food spot in which to search for
    * @param {string} tokens search tokens
-   * @returns Promise<result> matching search.json model
+   * @returns {Promise<FdcSearchResult>} matching search.json model
    */
-  search: (store, filtered, ...tokens) => {
+  search: (store, ...tokens) => {
     store = store.toLowerCase();
     return $.ajax({
       type: "POST",
@@ -44,7 +56,7 @@ const FdcSpec = {
       })
     }).then(result => {
       console.log("FDC API Search Result (raw)", result);
-      return new FdcSpec.SearchResult(result, store, filtered);
+      return new FdcSearchResult(result, store);
     });
   },
   getItem: itemID => {
@@ -52,10 +64,17 @@ const FdcSpec = {
       type: "GET",
       url: FdcSpec.itemUri(itemID),
       headers: { "Content-Type": "application/json" }
+    }).then(r => {
+      console.log("FDC API Item Get Result (raw)", r);
+      return new FdcFoodItem(r);
     });
   }
 };
 
-FdcSpec.search("Wendy's", true, "spicy", "chicken", "sandwich").then(sr =>
-  console.log(sr)
-);
+FdcSpec.search("Wendys", "spicy", "chicken", "sandwich")
+  .then(sr => FdcSpec.getItem(sr.results[0].id))
+  .then(item => {
+    console.time("X");
+    console.log(item, contains("lactose", item));
+    console.timeEnd("X");
+  });
